3a2f02a13327318d620f81f5170886063bd2cdf4
XxGonanxX <patomorelia@hotmail.com>
2023-08-02 23:11:48 -0600

    Now rust can work on mac and notes for mac added

---
+++ projects/hello_world/target/debug/deps/hello_world-4c4c3156900e3174.d
@@ -0,0 +1,5 @@
+/Users/alanpagzz/Desktop/Codes/Rust/Rust-coding/projects/hello_world/target/debug/deps/hello_world-4c4c3156900e3174: main.rs
+
+/Users/alanpagzz/Desktop/Codes/Rust/Rust-coding/projects/hello_world/target/debug/deps/hello_world-4c4c3156900e3174.d: main.rs
+
+main.rs:
--- projects/hello_world/target/debug/hello_world.d
+++ projects/hello_world/target/debug/hello_world.d
@@ -1,1 +1,1 @@
-C:\Users\patom\Desktop\Programming\Codigos\Rust\Rust-coding\projects\hello_world\target\debug\hello_world.exe: C:\Users\patom\Desktop\Programming\Codigos\Rust\Rust-coding\projects\hello_world\main.rs
+/Users/alanpagzz/Desktop/Codes/Rust/Rust-coding/projects/hello_world/target/debug/hello_world: /Users/alanpagzz/Desktop/Codes/Rust/Rust-coding/projects/hello_world/main.rs
